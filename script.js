1394380b53b7ce5748ddc654d3068a28bc417e51
Misha <michaelakrubova@gmail.com>
2022-10-08 00:21:25 +0200

    mobile menu completely responsive

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
- const navMenu = document.querySelector(".nav-menu");
- const navToggle = document.querySelector(".mobile-nav-toggle");
+const toggleBtn = document.querySelector(".mobile-nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const topBar = document.querySelector("header");
 
- navToggle.addEventListener("click", () => {
-    const visibility = navMenu.getAttribute("data-visible");
-    
-    if(visibility === "false") {
+
+window.addEventListener("DOMContentLoaded",()=> {
+    navMenu.classList.add("activated");
+})
+
+
+//menu is hiding on click enywhere
+document.addEventListener("click", (e) => {
+    console.log(e.target);
+    const isExpanded = toggleBtn.getAttribute("aria-expanded");
+
+    if(e.target.id === "toggleBtn" && isExpanded === "false") {
+        toggleBtn.setAttribute("aria-expanded", true);
         navMenu.setAttribute("data-visible", true);
-        navToggle.setAttribute("aria-expanded", true);
+    }
 
-    }
-    else  {
+
+    else if (e.target.id !== "toggleBtn" && e.target.id !== "nav-menu" && e.target.id !== "dropdown" && e.target.id !== "dropdown-link") {
+        toggleBtn.setAttribute("aria-expanded", false);
         navMenu.setAttribute("data-visible", false);
-        navToggle.setAttribute("aria-expanded", false) 
     }
- })
+})
+
+
