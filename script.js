be63fdcdbcba50581c31e55c9943ba696cd48d61
Misha <michaelakrubova@gmail.com>
2022-11-03 10:11:41 +0100

    Styling about section. Must solve problem with scrolling topBar

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+//topbar and navigation 
+
 const toggleBtn = document.querySelector(".mobile-nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
-const topBar = document.querySelector("header");
+const topBar = document.querySelector(".topbar");
 
 
+//mobile menu transition
 window.addEventListener("DOMContentLoaded",()=> {
     navMenu.classList.add("activated");
 })
 
-
-//menu is hiding on click enywhere
+//change color of topBar on scroll
+/*window.addEventListener("scroll", () => {
+    if(document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        topBar.classList.add("scrolled");
+    }
+    else {
+        topBar.classList.remove("scrolled");
+    }
+})
+*/
+//opening menu with click on menu icon and hiding on click on close icon or outside the menu
 document.addEventListener("click", (e) => {
     console.log(e.target);
     const isExpanded = toggleBtn.getAttribute("aria-expanded");
